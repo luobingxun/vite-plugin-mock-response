f6b2b3acdc0c57463e268a3315c653462e56b2a5
mingwei.li <mingwei.li@tarsocial.com>
2024-08-25 16:03:41 +0800

    feat: test

--- demo/vite.config.js
+++ demo/vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import mockResponse from '../src/index';
+// import mockResponse from '../src/index';
+import mockResponse from 'vite-plugin-mock-response';
 
 // https://vitejs.dev/config/
 export default defineConfig({
